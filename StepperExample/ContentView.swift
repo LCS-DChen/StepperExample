908c2e9030348227de4ea467f8258ccd5b0fa727
Deyana Chen <dchen@lcs.on.ca>
2026-04-20 14:24:10 -0400

    Initial Commit

---
+++ StepperExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepperExample
+//
+//  Created by chen tianyi on 2026-04-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by chen tianyi on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
